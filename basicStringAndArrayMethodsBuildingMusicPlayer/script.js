d0fb9b27dd85f7af78991dacbe8311e0502a0c35
Yuliya Kotova <yulya9449@yandex.ru>
2024-03-20 05:15:24 +0300

    feat: complete steps 36 - 38

--- basicStringAndArrayMethodsBuildingMusicPlayer/script.js
+++ basicStringAndArrayMethodsBuildingMusicPlayer/script.js
@@ -118,6 +118,14 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
+playButton.addEventListener('click', () => {
+  if (!userData?.currentSong) {
+    playSong(userData?.songs[0].id);
+  } else {
+    playSong(userData?.currentSong.id);
+  }
+});
+
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {
     if (a.title < b.title) {
